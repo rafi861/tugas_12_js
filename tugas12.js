8dbaa7b80fee7aea7aa35887dbcbe67531a8144e
Muhammad Rafi <muhammadrafi2545@gmail.com>
2023-03-10 17:41:30 +0700

    tugas 12 js

---
+++ tugas12.js
@@ -0,0 +1,13 @@
+function tugas12() {
+  var buah = ['pisang', 'jeruk', 'apel', 'mangga']
+  console.log(buah);
+  buah.pop()
+  console.log(buah);
+  // buah.shift()
+  // buah = buah.shift()
+  var buah2 = buah.shift()
+  return buah;
+
+}
+
+console.log(tugas12());
